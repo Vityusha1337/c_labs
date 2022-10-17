b59a3febb7cd38f9ea86b5dcb55cc67d23425d1e
user <you@example.com>
2022-10-17 14:49:04 +0500

    updated

--- 1.c
+++ 1.c
@@ -2,5 +2,31 @@
 #include <math.h>
 
 int main(){
-    int a, b 
+    int a, b, sum_a, sum_b, min, result;
+    sum_a = sum_b = 0;
+    printf("Enter a -> ");
+    scanf("%i", &a);
+    printf("Enter b -> ");
+    scanf("%i", &b);
+    while (a>=1){
+        sum_a+=(a%10);
+        a/=10;}
+    while (b>=1){
+        sum_b+=(b%10);
+        b/=10;}
+    if (sum_a!=sum_b){
+        if (sum_a<sum_b){
+            min = sum_a;
+            result = min/b;
+        }
+        else{
+            min = sum_b;
+            result = min/a;
+        }
+    }
+    else{
+        printf("Error. Parameters are equal");
+    }
+    printf ("%i", result);
+    return 0;
 }
